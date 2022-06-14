19d42ce1eadc949d2410d81bf367a7237d253f6f
ramdanwijaya <ramdan.zakat1112@gmail.com>
2022-06-14 19:53:26 +0700

    Initial commit

---
+++ app/src/main/java/com/example/app_dbsqlite/MataKuliah.kt
@@ -0,0 +1,8 @@
+package com.example.app_dbsqlite
+
+class MataKuliah (
+    var kdMatkul : String,
+    var NmMatkul : String,
+    var sks : Int,
+    var sifat: String,
+        )
---
+++ app/src/main/java/com/example/app_dbsqlite/layout_item.kt
@@ -0,0 +1,11 @@
+package com.example.app_dbsqlite
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class layout_item : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layout_item)
+    }
+}
